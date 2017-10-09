bd952351e198838d108222cc0bd67b1a10bd0089
sliri <kinglir@gmail.com>
2017-10-09 21:13:40 +0300

    Add files via upload

---
+++ sh_files/mybash.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+alias mycd='cd ~/jackal_navigation/src/my_pkg/maps/'
+mycd
+pwd
+mapname=$1
+echo $mapname
+mapname1=$mapname.png
+mapname2=$mapname.pgm
+mapname3=$mapname.txt
+#terminator -e "rostopic echo -n 1 /gazebo/model_states > $mapname.txt"& ## big mistake!!!
+mkdir -p $mapname
+cd $mapname
+terminator -e "rostopic echo -n 1 /gazebo/link_states > $mapname.txt"&
+sleep 2s
+#terminator -e "rosrun my_pkg ground_truth2b.py $mapname1 $mapname2 $mapname3"&
+#terminator -e "rosrun my_pkg ground_truth3.py $mapname1 $mapname2 $mapname3"&
+#terminator -e "rosrun my_pkg ground_truth5.py $mapname1 $mapname2 $mapname3"&
+terminator -e "rosrun my_pkg ground_truth6.py $mapname1 $mapname2 $mapname3"&
+
+sleep 10s
+cp -n /home/liron/jackal_navigation/src/jackal_simulator/jackal_gazebo/launch/$mapname.launch $mapname.launch 
+cp -n /home/liron/jackal_navigation/src/jackal_simulator/jackal_gazebo/launch/jackal.launch jackal.launch 
+cp -n /home/liron/jackal_navigation/src/jackal_simulator/jackal_gazebo/worlds/$mapname.world $mapname.world 
+cp -n /home/liron/jackal_navigation/src/my_pkg/scripts/cone_world1.yaml $mapname.yaml 
+
+#yaml_text="image: $mapname.png\n
+#		   resolution: 0.025688073\n
+#		   origin: [-56.000000, -56.000000, 0.000000]\n
+#		   negate: 0\n
+#		   occupied_thresh: 0.65\n
+#		   free_thresh: 0.196\n "
+
+
+#yaml_text="image: $mapname.png\n
+#		   resolution: 0.02564102564\n
+#		   origin: [-40.000000, -40.000000, 0.000000]\n
+#		   negate: 0\n
+#		   occupied_thresh: 0.65\n
+#		   free_thresh: 0.196\n "
+		   
+
+#yaml_text="image: $mapname.png\n
+#		   resolution: 0.02565597668\n
+#		   origin: [-44.000000, -44.000000, 0.000000]\n
+#		   negate: 0\n
+#		   occupied_thresh: 0.65\n
+#		   free_thresh: 0.196\n "
+
+
+yaml_text="image: $mapname.png\n
+		   resolution: 0.0255863539\n
+		   origin: [-30.000000, -30.000000, 0.000000]\n
+		   negate: 0\n
+		   occupied_thresh: 0.65\n
+		   free_thresh: 0.196\n "		   
+
+
+
+echo $yaml_text > $mapname.yaml 
+
+sleep 10s
---
+++ sh_files/run_jacakl.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+killall gzserver
+killall -9 gazebo
+sleep 8s
+
+#mapname=cone_orcahrd4_single_cone_dz2
+#mapname=cone_orcahrd4_single_cone_dy
+#mapname=cone_orcahrd4a
+#mapname=cone_orcahrd4_interleaving_dy3
+#mapname=cone_orcahrd4_unique_pattern_cones_dy
+#mapname=cone_orchard4_one_rect
+mapname=cone_orcahrd5b #Two Cones missing
+#mapname=cone_orcahrd5e #3 Cones missing
+#mapname=cone_orcahrd5c #Half row missing
+#mapname=cone_orcahrd5d #single cone missing
+#mapname=cone_orcahrd6a #two cone 1 m ahead
+#mapname=cone_orcahrd6b #half row ahead
+#mapname=cone_orcahrd6c #single cylinder 
+#mapname=cone_orcahrd6d #3 cylinders near axis 
+
+export JACKAL_LASER=1
+export JACKAL_LASER_SCAN_TOPIC=front/scan
+
+
+
+##create bag
+###################################
+create_bag=false
+#create_bag=false
+
+
+##create map
+###################################
+#create_map=true 
+create_map=false 
+
+#headless
+###################################
+###see http://gazebosim.org/tutorials?tut=ros_roslaunch
+##see  http://gazebosim.org/tutorials?tut=quick_start
+#headless=true
+headless=false
+
+
+#inject pose
+###################################
+inject_initial_pose=True
+#inject_initial_pose=False
+xinitial=8.0 #initial x for AMCL
+yinitial=8.0 #initial y for AMCL
+
+######################################
+##launching
+######################################
+##########################################################################
+
+if $headless; then
+terminator -e "roslaunch jackal_gazebo $mapname.launch gui:=false headless:=true"&
+sleep 10s
+terminator -e "roslaunch jackal_gazebo jackal.launch gui:=false headless:=true"&
+sleep 10s
+terminator -e "roslaunch jackal_navigation odom_navigation_demo.launch"&
+sleep 5s
+
+if $create_map; then
+	sleep 60s
+	sh mybash.sh $mapname
+fi
+
+
+terminator -e "roslaunch jackal_navigation amcl_demo.launch map_file:=/home/liron/jackal_navigation/src/my_pkg/maps/$mapname/$mapname.yaml"&
+sleep 3s
+#terminator -e "roslaunch jackal_viz view_robot.launch config:=localization"&
+
+
+
+else
+#gnome-terminal -e "roslaunch gazebo_ros empty_world.launch"&
+
+gnome-terminal -e "roslaunch jackal_gazebo $mapname.launch gui:=true headless:=false"&
+sleep 20s
+#gnome-terminal -e "roslaunch jackal_gazebo jackal.launch gui:=true headless:=false"&
+#gnome-terminal  -e "roslaunch jackal_navigation odom_navigation_demo.launch"&
+sleep 5s
+
+if $create_map; then
+	sleep 60s
+	sh mybash.sh $mapname
+fi
+
+gnome-terminal  -e "rosrun map_server map_server /home/liron/jackal_navigation/src/my_pkg/maps/$mapname/$mapname.yaml respawn=true"&
+sleep 10s
+gnome-terminal  -e "roslaunch jackal_navigation amcl_demo.launch map_file:=/home/liron/jackal_navigation/src/my_pkg/maps/$mapname/$mapname.yaml use_map_topic:=true"&
+sleep 6s
+gnome-terminal  -e "roslaunch jackal_viz view_robot.launch config:=localization"&
+
+fi
+
+##########################################################################
+
+
+
+##ruunning scripts
+#####################################
+
+cd /home/liron/jackal_navigation/src/my_pkg/maps/$mapname
+
+
+sleep 5s
+
+gnome-terminal  -e "rosrun my_pkg doitall_node2.py $xinitial $yinitial $inject_initial_pose"&
+
+sleep 6s
+
+
+
+if $create_bag; then
+gnome-terminal -e "rosrun teleop_twist_keyboard teleop_twist_keyboard.py"&
+gnome-terminal -e "roslaunch jackal_gazebo bag_record.launch"&
+
+#rosbag reindex $mapname.bag
+fi
+
+sleep 5s
+
+terminator -e "rosrun my_pkg cmd_vel_based_patrol5.py"&
+sleep 2s
+terminator -e  "rosservice call --wait /global_localization"&
+#gnome-terminal -e "roslaunch jackal_gazebo bag_play.launch"&
+
+#terminator -e "rqt_graph"&
+#terminator -e "rqt_plot /amcl_pose/pose/pose/position/x"& 
+#terminator -e "rqt_plot /amcl_pose/pose/pose/covariance[1]"& 
+#rqt_plot /amcl_pose/pose/pose/covariance[3]& 
+#terminator -e "rostopic echo -n 20 /gazebo/link_states > link_states.txt"&
+
+
+
+
+
+
+
